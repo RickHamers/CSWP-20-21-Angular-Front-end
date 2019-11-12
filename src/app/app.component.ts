61577cc83feed8fa41835487ffe7074530215287
Rick Hamers <raw.hamers@student.avans.nl>
2019-11-12 20:56:09 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cswp-angular-nr2128706-yr1920';
+}
