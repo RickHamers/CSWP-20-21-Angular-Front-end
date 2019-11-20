3329447e1e3efa2de33aa0b0e056aeefea3f6f2c
Rick Hamers <raw.hamers@student.avans.nl>
2019-11-20 15:46:38 +0100

    initial profile picture functionality
    initial profile picture functionality

--- src/app/components/account/profile/profile.component.ts
+++ src/app/components/account/profile/profile.component.ts
@@ -1,15 +1,25 @@
+import { environment } from './../../../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { FileUploader, FileSelectDirective } from 'ng2-file-upload'
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
+
 export class ProfileComponent implements OnInit {
+
+  public uploader: FileUploader = new FileUploader({url: environment.apiUrl + '/profile/photo', itemAlias: 'profilePicture'})
 
   constructor() { }
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      console.log('FileUpload:uploaded:', item, status, response);
+      alert('File uploaded succesfully');
+    }
   }
 
 }
