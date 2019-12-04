da4f7932b2993b45bcaaf08a7788d222d1d01ef7
Rick Hamers <Rick.Hamers@cmtelecom.com>
2019-12-04 10:35:53 +0100

    Remove console.log which was no longer needed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: /*'https://cswp-19-20-nodejs-api-2128706.herokuapp.com',*/ 'http://localhost:3000',
+  apiUrl: 'https://cswp-19-20-nodejs-api-2128706.herokuapp.com',
   production: true
 };
+
