dc10c7690f9f7108b14233f9aa19a4eeedd40ca3
TypicalDoofus <38530785+RickHamers@users.noreply.github.com>
2020-12-08 14:53:30 +0100

    Update git URLs
    Update Git URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://cswp-19-20-nodejs-api-2128706.herokuapp.com',
+  apiUrl: 'https://cswp-20-21-nodejs-api-2128706.herokuapp.com',
   production: true
 };
 
